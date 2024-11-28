f1e0a5817a5c4d2728f2f1678367a826bf70fcec
Thayná Simões <thayna.pereira2402@gmail.com>
2024-11-28 13:28:58 -0300

    corrige indentacao da classe Postagem

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -13,55 +13,53 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name="tb_postagens")
+@Table(name = "tb_postagens")
 public class Postagem {
-	
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
 
-@NotBlank(message = "O atributo t´ritulo é Obrigatório!")
-@Size(min = 5, max = 100, message = "O atributo deve conter no mínimo 05 e no máximo 100 caracteres")
-private String titulo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-@NotBlank(message = "O atributo texto é Obrigatório!")
-@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
-private String texto;
+    @NotBlank(message = "O atributo título é Obrigatório!")
+    @Size(min = 5, max = 100, message = "O atributo deve conter no mínimo 05 e no máximo 100 caracteres")
+    private String titulo;
 
-@UpdateTimestamp
-private LocalDateTime data;
+    @NotBlank(message = "O atributo texto é Obrigatório!")
+    @Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+    private String texto;
 
-public Long getId() {
-	return id;
-}
+    @UpdateTimestamp
+    private LocalDateTime data;
 
-public void setId(Long id) {
-	this.id = id;
-}
+    public Long getId() {
+        return id;
+    }
 
-public String getTitulo() {
-	return titulo;
-}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-public void setTitulo(String titulo) {
-	this.titulo = titulo;
-}
+    public String getTitulo() {
+        return titulo;
+    }
 
-public String getTexto() {
-	return texto;
-}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-public void setTexto(String texto) {
-	this.texto = texto;
-}
+    public String getTexto() {
+        return texto;
+    }
 
-public LocalDateTime getData() {
-	return data;
-}
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 
-public void setData(LocalDateTime data) {
-	this.data = data;
-}
+    public LocalDateTime getData() {
+        return data;
+    }
 
-
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 }
