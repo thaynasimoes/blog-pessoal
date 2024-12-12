dc5b1e4e34f73bb8184be5c99cdeefdf51529674
Thayná Simões <thayna.pereira2402@gmail.com>
2024-12-12 20:43:24 -0300

    adiciona testes

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -43,8 +43,17 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
-	/* Insira os Getters and Setters */
 
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() { }
+	
 	public Long getId() {
 		return this.id;
 	}
